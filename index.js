fe79ec55438ef21cd6b1f3f78e3164a81c157b32
dieguitojd <diegoaw591@gmail.com>
2022-01-05 00:25:01 -0600

    add dotenv

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
-const url = "mongodb+srv://diegojv:Montypython5302@marvel.lsbia.mongodb.net/restapi?retryWrites=true&w=majority"
+
+
 //connecting to mongo
 mongoose.Promise = global.Promise;
 
-mongoose.connect(url, {
+mongoose.connect(process.env.URL, {
     useNewUrlParser: true
 });
 
